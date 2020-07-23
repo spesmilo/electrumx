be223584cb4f4bb3363a2e2c0f5016bd596d1873
Paulius Stakauskas <paulius@zenlot.xyz>
2020-07-23 14:28:36 +0100

    Typo in entrypoint

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 if [[ "$1" = "electrumx_server" ]]; then
     exec gosu electrumx "$@"
 elif [[ "$1" == "electrumx_rpc" ]]; then
-    exec gosu electrumx "$@"
+    exec gosu electrumx_rpc "$@"
 else
     exec "$@"
 fi

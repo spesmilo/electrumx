8b7efde6e6394226a7f7bb444a47d640c9841c8b
SomberNight <somber.night@protonmail.com>
2021-01-19 16:24:51 +0100

    session: wait for protocol ver negotiation to finish before other reqs
    We already enforce that server.version must be the first received message
    in a session, but we also need to ensure that the server finishes processing
    that message and sets up the correct protocol version before starting to
    process further messages.

--- electrumx/server/session.py
+++ electrumx/server/session.py
@@ -909,6 +909,7 @@ class SessionBase(RPCSession):
         self.coin = self.env.coin
         self.client = 'unknown'
         self.sv_seen = False  # has seen 'server.version' message?
+        self.sv_negotiated = asyncio.Event()  # done negotiating protocol version
         self.anon_logs = self.env.anon_logs
         self.txs_sent = 0
         self.log_me = SessionBase.log_new
@@ -989,6 +990,9 @@ class SessionBase(RPCSession):
             self.logger.info(f'closing session: server.version must be first msg. got: {method}')
             await self._do_crash_old_electrum_client()
             raise ReplyAndDisconnect(BAD_REQUEST, f'use server.version to identify client')
+        # Wait for version negotiation to finish before processing other messages.
+        if method != 'server.version' and not self.sv_negotiated.is_set():
+            await self.sv_negotiated.wait()
 
         self.session_mgr._method_counts[method] += 1
         coro = handler_invocation(handler, request)()
@@ -1546,6 +1550,7 @@ class ElectrumX(SessionBase):
                 BAD_REQUEST, f'unsupported protocol version: {protocol_version}'))
         self.set_request_handlers(ptuple)
 
+        self.sv_negotiated.set()
         return electrumx.version, self.protocol_version_string()
 
     async def transaction_broadcast(self, raw_tx):
@@ -1688,6 +1693,7 @@ class LocalRPC(SessionBase):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.sv_seen = True
+        self.sv_negotiated.set()
         self.client = 'RPC'
         self.connection.max_response_size = 0
 

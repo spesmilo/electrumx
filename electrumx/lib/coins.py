e5be71e3a240fb27377cf8663aeb45b3d840419c
Michel van Kessel <michelvankessel@protonmail.com>
2021-03-06 22:35:05 +0100

    update Blackcoin (BLK) and added testnet
    mainnet and testnet confirmed working on Ubuntu 20.04 with Python3.8.5

--- electrumx/lib/coins.py
+++ electrumx/lib/coins.py
@@ -1617,18 +1617,37 @@ class Blackcoin(ScryptMixin, Coin):
     NAME = "Blackcoin"
     SHORTNAME = "BLK"
     NET = "mainnet"
+    XPUB_VERBYTES = bytes.fromhex("0488B21E")
+    XPRV_VERBYTES = bytes.fromhex("0488ADE4")
     P2PKH_VERBYTE = bytes.fromhex("19")
     P2SH_VERBYTES = (bytes.fromhex("55"),)
     WIF_BYTE = bytes.fromhex("99")
     GENESIS_HASH = ('000001faef25dec4fbcf906e6242621d'
                     'f2c183bf232f263d0ba5b101911e4563')
-    DAEMON = daemon.LegacyRPCDaemon
-    TX_COUNT = 4594999
-    TX_COUNT_HEIGHT = 1667070
+    TX_COUNT = 10802426
+    TX_COUNT_HEIGHT = 3431329
     TX_PER_BLOCK = 3
     RPC_PORT = 15715
-    REORG_LIMIT = 5000
+    REORG_LIMIT = 500
 
+
+class BlackcoinTestnet(Blackcoin):
+    NAME = "BlackcoinTestnet"
+    SHORTNAME = "tBLK"
+    NET = "testnet"
+    XPUB_VERBYTES = bytes.fromhex("043587CF")
+    XPRV_VERBYTES = bytes.fromhex("04358394")
+    P2PKH_VERBYTE = bytes.fromhex("6F")
+    P2SH_VERBYTES = (bytes.fromhex("C4"),)
+    WIF_BYTE = bytes.fromhex("EF")
+    GENESIS_HASH = ('0000724595fb3b9609d441cbfb957761'
+                    '5c292abf07d996d3edabc48de843642d')
+    TX_COUNT = 1401229
+    TX_COUNT_HEIGHT = 698611
+    TX_PER_BLOCK = 2
+    RPC_PORT = 25715
+    REORG_LIMIT = 500
+    
 
 class Bitbay(ScryptMixin, Coin):
     NAME = "Bitbay"

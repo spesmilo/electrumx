221bd3da5409aab933f109854e43c5a0f9ebed72
chenshane2013@gmail.com <sean>
2024-06-03 17:15:26 +0800

    fix

--- electrumx/server/http_session.py
+++ electrumx/server/http_session.py
@@ -2,11 +2,10 @@
 
 from aiohttp import web
 import electrumx.lib.util as util
-
+from electrumx.lib.hash import hash_to_hex_str
 
 
 class HttpHandler(object):
-
     PROTOCOL_MIN = (1, 4)
     PROTOCOL_MAX = (1, 4, 3)
 
@@ -15,12 +14,17 @@ class HttpHandler(object):
         self.logger = util.class_logger(__name__, self.__class__.__name__)
         self.db = db
 
-
     async def all_utxos(self, request):
         startkey = request.match_info.get('startkey', '')
         limit = request.match_info.get('limit', '')
         last_db_key, utxos = await self.db.pageable_utxos(startkey, limit)
-        res= {'last_db_key': last_db_key, 'utxos': utxos}
+        data_list = []
+        for utxo in utxos:
+            data = {'height': utxos.height,
+                    'txid': hash_to_hex_str(utxo.tx_hash),
+                    'vout': utxo.tx_pos,
+                    'value': utxo.value}
+            data_list.append(data)
+
+        res = {'last_db_key': last_db_key, 'utxos': data_list}
         return web.json_response(res)
-
-

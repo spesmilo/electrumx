5880e75c38a4d40359ff9d5808d7571a017a58a3
SomberNight <somber.night@protonmail.com>
2025-04-16 15:04:51 +0000

    prepare release 1.17.0

--- src/electrumx/__init__.py
+++ src/electrumx/__init__.py
@@ -2,6 +2,6 @@ from electrumx.server.controller import Controller
 from electrumx.server.env import Env
 
 
-__version__ = "1.16.0"
+__version__ = "1.17.0"
 version = f'ElectrumX {__version__}'
 version_short = __version__

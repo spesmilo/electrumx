d4383b5318e9aa7f2809cf2cbca4eeacded980d6
SomberNight <somber.night@protonmail.com>
2026-06-25 16:45:36 +0000

    bp: advance_txs: use threadpool for processing tx-inputs
    spend_utxo() does disk reads, so previously any single disk read
    would block the whole of advance_txs().

--- src/electrumx/server/block_processor.py
+++ src/electrumx/server/block_processor.py
@@ -10,6 +10,7 @@
 
 
 import asyncio
+from concurrent.futures import ThreadPoolExecutor
 import time
 from typing import Sequence, Tuple, List, Callable, Optional, TYPE_CHECKING, Type, Set
 
@@ -467,7 +468,6 @@ class BlockProcessor:
             txs: Sequence[Tx],
             is_unspendable: Callable[[bytes], bool],
     ) -> Sequence[bytes]:
-        # TODO split into two funcs, first fund all outputs, second spend all inputs
         self.txids_rev.append(b''.join(tx.txid_rev for tx in txs))
 
         # Use local vars for speed in the loops
@@ -483,7 +483,7 @@ class BlockProcessor:
         _pack_sats = pack_satoshis_val
         _pack_txnum = pack_txnum
 
-        # Add the new UTXOs
+        # 1. process tx outputs: fund UTXOs
         for tx, hashXs in zip(txs, hashXs_by_tx):
             txid_rev = tx.txid_rev
             add_hashXs = hashXs.append
@@ -502,9 +502,9 @@ class BlockProcessor:
                 add_touched_outpoint((txid_rev, idx))
             tx_num += 1
 
-        # Spend the inputs
-        # A separate for-loop here allows any tx ordering in block.
-        for tx_pos, tx in enumerate(txs):
+        # 2. process tx inputs: spend UTXOs
+        # note: we don't care about tx ordering in the block
+        def process_txins_for_single_tx(tx_pos: int, tx: Tx) -> None:
             add_hashXs = hashXs_by_tx[tx_pos].append
             tx_undo_info = []  # type: list[bytes]
             tx_undo_info_append = tx_undo_info.append
@@ -517,6 +517,15 @@ class BlockProcessor:
                 prevout_tuple = (txin.prev_txid_rev, txin.prev_idx)
                 add_touched_outpoint(prevout_tuple)
             bl_undo_info[tx_pos] = b"".join(tx_undo_info)
+
+        def process_txins_for_chunk(txs_chunk):
+            for tx_pos, tx in txs_chunk:
+                process_txins_for_single_tx(tx_pos=tx_pos, tx=tx)
+
+        list(self.pool_executor.map(
+            process_txins_for_chunk,
+            chunks(list(enumerate(txs)), 200),
+        ))
 
         # Update touched set for notifications
         for hashXs in hashXs_by_tx:
@@ -756,15 +765,16 @@ class BlockProcessor:
         '''
         self._caught_up_event = caught_up_event
         await self._first_open_dbs()
-        try:
-            async with OldTaskGroup() as group:
-                await group.spawn(self.prefetcher.main_loop(self.height))
-                await group.spawn(self._process_prefetched_blocks())
-        # Don't flush for arbitrary exceptions as they might be a cause or consequence of
-        # corrupted data
-        except CancelledError:
-            self.logger.info('flushing to DB for a clean shutdown...')
-            await self.flush(True)
+        with ThreadPoolExecutor() as self.pool_executor:
+            try:
+                async with OldTaskGroup() as group:
+                    await group.spawn(self.prefetcher.main_loop(self.height))
+                    await group.spawn(self._process_prefetched_blocks())
+            # Don't flush for arbitrary exceptions as they might be a cause or consequence of
+            # corrupted data
+            except CancelledError:
+                self.logger.info('flushing to DB for a clean shutdown...')
+                await self.flush(True)
 
     def force_chain_reorg(self, count: int) -> bool:
         '''Force a reorg of the given number of blocks.
